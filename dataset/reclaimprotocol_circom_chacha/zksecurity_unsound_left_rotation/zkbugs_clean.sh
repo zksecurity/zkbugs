1bdc1893e29cca22d9038b5a655b221e5157a88f
Stefanos Chaliasos <stefanos@chaliasos.com>
2024-07-30 19:04:38 +0300

    We don't need public.json

--- dataset/reclaimprotocol_circom_chacha/zksecurity_unsound_left_rotation/zkbugs_clean.sh
+++ dataset/reclaimprotocol_circom_chacha/zksecurity_unsound_left_rotation/zkbugs_clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rm -rf circuit.sym circuit_0001.zkey circuit.r1cs circuit_0000.zkey circuit_js final.ptau proof.json verification_key.json detect.sage.py witness.json circuit.json
+rm -rf circuit.sym circuit_0001.zkey circuit.r1cs circuit_0000.zkey circuit_js \
+    final.ptau proof.json verification_key.json detect.sage.py witness.json \
+    circuit.json public.json
